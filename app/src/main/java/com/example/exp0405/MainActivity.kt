517714c563b4a85ce506b35752cfada86df02915
emmmmYeah <1776283041@qq.com>
2022-04-12 23:10:02 +0800

    可以实现增删改的基本功能，但是在删和改的时候会出现越界报错，应该是存储数据的时候，操作顺序的问题，还需进一步调试

--- app/src/main/java/com/example/exp0405/MainActivity.kt
+++ app/src/main/java/com/example/exp0405/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.exp0405
 
+import android.annotation.SuppressLint
 import android.content.ContentValues
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -44,11 +45,13 @@ class MainActivity : AppCompatActivity() {
     private fun saveInput() {
         //取出字符串并转向saveInDb()中
         val text=findViewById<EditText>(R.id.ipt_text).text.toString()
+        Log.d(TAG,"save in edit=$text")
         saveInDb(text)
     }
 
     private fun saveInDb(text: String) {
         //往数据库存数据！！！
+        Log.d(TAG,"get in edit=$text")
         val db=helper.writableDatabase
         val item=Todo(text,System.currentTimeMillis())
         val values=ContentValues().apply {
@@ -62,30 +65,35 @@ class MainActivity : AppCompatActivity() {
             Log.d(TAG,"UPDATE ID=$rs")
             rs=db.update(Todo.TABLE,values,"id=?", arrayOf(toUpdate?.id.toString()))
             if(rs!=-1){
+
                 toUpdate?.id?.let { adapter.replaceItem(it,item) }
                 toUpdate=null
+            }
         }
             else{
-                Log.d(TAG,"insert id =$rs")
+            Log.d(TAG,"insert id =$rs")
                 rs=db.insert(Todo.TABLE,null,values).toInt()
+            Log.d(TAG,"insert id =$rs")
                 if(rs!=-1){
                     item.id=rs
                     adapter.addItem(item)
                 }
             }
-        }
+            //一定要置空！！
+            setInputText("")
 
-        //一定要置空！！
-        setInputText("")
+
+
 
     }
 
     private fun setInputText(s: String) {
         findViewById<EditText>(R.id.ipt_text).setText(s)
+        Log.d(TAG,"text=$s")
 
     }
 
-
+    @SuppressLint("Range")
     private fun readInDb() {
         val db = helper.readableDatabase//文件为只读
         val cursor = db.query(Todo.TABLE,null,null,null,null,null,
@@ -95,13 +103,18 @@ class MainActivity : AppCompatActivity() {
             do{//遍历Cursor对象，取出数据并打印
                 arr.add(
                     Todo(
-                        //这里需要加个判断才行
-                        cursor.getString(cursor.getColumnIndex(Todo.COL_CONTENT)),
-                        cursor.getLong(cursor.getColumnIndex(Todo.COL_TIME)),
-                    ).apply {
-                        id = cursor.getInt(cursor.getColumnIndex(Todo.COL_ID))
+                        //这里需要加个判断才行,加了也不得行，所以加了这个函数上面那个注解
+
+                            cursor.getString(cursor.getColumnIndex(Todo.COL_CONTENT)),
+                            cursor.getLong(cursor.getColumnIndex(Todo.COL_TIME)),
+                        ).apply {
+                        if(cursor.getColumnIndex(Todo.COL_ID)>=0){
+                            id = cursor.getInt(cursor.getColumnIndex(Todo.COL_ID))
+                        }
+                        Log.d(TAG,"id ${Todo.COL_ID},cont ${Todo.COL_CONTENT}")
                     }
                 )
+
             }while (cursor.moveToNext())
         }
 
@@ -110,36 +123,36 @@ class MainActivity : AppCompatActivity() {
         cursor.close()
     }
 //这个是使用recycler最重要的部分
-  inner class TodoAdapter: RecyclerView.Adapter<TodoViewHolder>() {
-    val cont= arrayListOf<Todo>()//这个是getItemCount重要返还的数据大小的数据
+  inner class TodoAdapter: RecyclerView.Adapter<TodoAdapter.TodoViewHolder>() {
+    val cont = arrayListOf<Todo>()//这个是getItemCount重要返还的数据大小的数据
 
-        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TodoViewHolder {
-           //在实际开发种LayoutInflater这个类还是非常有用的，它的作用类似于findViewById()，不同点是LayoutInflater是用来找layout下xml布局文件，并且实例化！而findViewById()是找具体xml下的具体widget控件(如:Button,TextView等)。
-           val view= LayoutInflater.from(parent.context).inflate(R.layout.item_todo,parent,false)
-            return TodoViewHolder(view).apply {
-                id=view.findViewById(R.id.id)
-                content=view.findViewById(R.id.content)
-                btnUpdate=view.findViewById(R.id.btn_update)
-                btnDelete=view.findViewById(R.id.btn_delete)
-            }
-            //  val holder=TeacherViewHolder(view)EXP0320中的写法,
-            //        return holder
-            //下面是EXP0320中TeacherViewHolder中的内容，和我这个有区别在于在HOlder中就已经找好val值了而我这里是在OnCreat里才找的
-            //val name=itemView.findViewById<TextView>(R.id.name)
-            //         val desc=itemView.findViewById<TextView>(R.id.description)
-            //         val img=itemView.findViewById<ImageView>(R.id.avater)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TodoViewHolder {
+        //在实际开发种LayoutInflater这个类还是非常有用的，它的作用类似于findViewById()，不同点是LayoutInflater是用来找layout下xml布局文件，并且实例化！而findViewById()是找具体xml下的具体widget控件(如:Button,TextView等)。
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_todo, parent, false)
+        return TodoViewHolder(view).apply {
+            id = view.findViewById(R.id.id)
+            content = view.findViewById(R.id.content)
+            btnUpdate = view.findViewById(R.id.btn_update)
+            btnDelete = view.findViewById(R.id.btn_delete)
+        }
+        //  val holder=TeacherViewHolder(view)EXP0320中的写法,
+        //        return holder
+        //下面是EXP0320中TeacherViewHolder中的内容，和我这个有区别在于在HOlder中就已经找好val值了而我这里是在OnCreat里才找的
+        //val name=itemView.findViewById<TextView>(R.id.name)
+        //         val desc=itemView.findViewById<TextView>(R.id.description)
+        //         val img=itemView.findViewById<ImageView>(R.id.avater)
 //这个item需要我们用inflate函数把msg_item动态的加载进main布局，
 //并且返回了一个用来获取item里控件并且对其进行操作的View对象。
-        }
+    }
 
-        override fun onBindViewHolder(holder: TodoViewHolder, position: Int) {
-           //束缚控件的位置
-            holder.render(cont[position])
-        }
+    override fun onBindViewHolder(holder: TodoViewHolder, position: Int) {
+        //束缚控件的位置
+        holder.render(cont[position])
+    }
 
-        override fun getItemCount(): Int {
-            return cont.size
-        }
+    override fun getItemCount(): Int {
+        return cont.size
+    }
 
     fun setData(arr: ArrayList<Todo>) {
         cont.addAll(arr)
@@ -148,47 +161,68 @@ class MainActivity : AppCompatActivity() {
 
     fun replaceItem(id: Int, item: Todo) {
         val idx = findIdx(id)
-        if(idx >= 0) {
+        if (idx >= 0) {
+            Log.d(TAG,"idx=$idx,item=$item")
             cont.set(idx, item)
             notifyItemChanged(idx)
         }
     }
 
     fun addItem(item: Todo) {
-        cont.add(0,item)
+        cont.add(0, item)
         notifyItemInserted(0)
     }
-    private fun findIdx(id:Int?):Int{
-        var idx=-1
-        cont.forEachIndexed{ index,todo-?
+
+    private fun findIdx(id: Int?): Int {
+        var idx = -1
+        cont.forEach{
+            if(it.id==id){
+                idx= it.id!!
+            }
+        }
+        /*  cont.forEachIndexed{ index,todo-?
             if(todo.id==id){
                 idx=index
             }
         }
         return idx
+    }*/
+        return idx
+
     }
 
-}
+    inner class TodoViewHolder(view: View) : RecyclerView.ViewHolder(view) {
 
-    inner class TodoViewHolder(view: View):RecyclerView.ViewHolder(view) {
+        var id: TextView? = null
+        var content: TextView? = null
+        var btnUpdate: TextView? = null
+        var btnDelete: TextView? = null
+        fun render(todo: Todo) {
+            id?.text = todo.id.toString()
+            content?.text = todo.content
 
-        var id:TextView?=null
-        var content:TextView?=null
-        var btnUpdate:TextView?=null
-        var btnDelete:TextView?=null
-        fun render(todo:Todo) {
-            id?.text=todo.id.toString()
-            content?.text=todo.content
-
-            btnDelete?.setOnClickListener{
+            btnDelete?.setOnClickListener {
+                Log.d(TAG,"to delete id=${todo.id}")
+                val db=helper.writableDatabase
+                db.delete(Todo.TABLE,"id=?", arrayOf(todo.id.toString()))
+                adapter.itemDeleted(todo.id)
 
             }
             btnUpdate?.setOnClickListener {
-
+Log.d(TAG,"to Update id=${todo.id}")
+                toUpdate=todo
+                setInputText(todo.content)
             }
         }
     }
 
+    private fun itemDeleted(id: Int?) {
+        val idx=findIdx(id)
+        if(idx>=0) {
+            cont.removeAt(idx)
+            notifyItemRemoved(idx)
+        }
+    }
 
-
-}
+//tas
+}}
